c78bae397041e77cb6dae29b62dab7649dfd8c83
tarrinros <tarindis@gmail.com>
2019-04-20 18:36:06 +0300

    Triangle first iteration

--- triangle/triangle.rb
+++ triangle/triangle.rb
@@ -1,14 +1,44 @@
 class Triangle
   def initialize(sides)
-    @sides = sides if triangle?(sides)
+    @sides = sides
+    @a_side = sides[0]
+    @b_side = sides[1]
+    @c_side = sides[2]
   end
 
-  def triangle?(sides)
+  def is_triangle?(sides)
     min_sides = sides.min(2)
-    sides.all? { |side| side > 0 } && sides.max < min_sides[0] + min_sides[1]
+
+    sides.count == 3 &&
+      sides.all? { |side| side > 0 } &&
+      sides.max <= min_sides.sum
   end
 
   def equilateral?
-    a_side = @sides.max
+    if is_triangle?(@sides)
+      biggest_side = @sides.max
+
+      @sides.all? { |side| side == biggest_side }
+    else
+      return false
+    end
+  end
+
+  def isosceles?
+    if is_triangle?(@sides)
+      equal_sides = @sides.select{ |side| @sides.count(side) > 1 }
+
+      equal_sides.count >= 2 ? true : false
+    else
+      return false
+    end
+  end
+
+  def scalene?
+    if is_triangle?(@sides)
+      @sides.uniq.count == 3 ? true : false
+    else
+      return false
+    end
   end
 end
