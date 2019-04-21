4a4873cdf51f27162ad5f3f584821c414f584035
tarrinros <tarindis@gmail.com>
2019-04-21 23:49:46 +0300

    Space age add solar year hash

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,46 +1,54 @@
 class SpaceAge
   EARTH_YEAR_IN_SECONDS = 31557600.to_f
-  MERCURY_YEAR_IN_SECONDS = 0.2408467 * EARTH_YEAR_IN_SECONDS
-  VENUS_YEAR_IN_SECONDS = 0.61519726 * EARTH_YEAR_IN_SECONDS
-  MARS_YEAR_IN_SECONDS = 1.8808158 * EARTH_YEAR_IN_SECONDS
-  JUPITER_YEAR_IN_SECONDS = 11.862615 * EARTH_YEAR_IN_SECONDS
-  SATURN_YEAR_IN_SECONDS = 29.447498 * EARTH_YEAR_IN_SECONDS
-  URANUS_YEAR_IN_SECONDS = 84.016846 * EARTH_YEAR_IN_SECONDS
-  NEPTUNE_YEAR_IN_SECONDS = 164.79132 * EARTH_YEAR_IN_SECONDS
+  solar_years = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
 
-  def initialize(args)
-    @args = args
+  ["mercury", "venus", "mars", "jupiter", "saturn", "uranus", "neptune"].each do |action|
+    define_method("perform_#{action}") do |argument|
+      "performing #{action.gsub('_', ' ')} on #{argument}"
+    end
   end
 
-  def on_earth
-    (@args / EARTH_YEAR_IN_SECONDS).round(2)
+  def initialize(seconds)
+    @seconds = seconds
   end
 
-  def on_mercury
-    (@args / MERCURY_YEAR_IN_SECONDS).round(2)
-  end
-
-  def on_venus
-    (@args / VENUS_YEAR_IN_SECONDS).round(2)
-  end
-
-  def on_mars
-    (@args / MARS_YEAR_IN_SECONDS).round(2)
-  end
-
-  def on_jupiter
-    (@args / JUPITER_YEAR_IN_SECONDS).round(2)
-  end
-
-  def on_saturn
-    (@args / SATURN_YEAR_IN_SECONDS).round(2)
-  end
-
-  def on_uranus
-    (@args / URANUS_YEAR_IN_SECONDS).round(2)
-  end
-
-  def on_neptune
-    (@args / NEPTUNE_YEAR_IN_SECONDS).round(2)
-  end
+  # def on_earth
+  #   (@args / EARTH_YEAR_IN_SECONDS).round(2)
+  # end
+  #
+  # def on_mercury
+  #   (@args / MERCURY_YEAR_IN_SECONDS).round(2)
+  # end
+  #
+  # def on_venus
+  #   (@args / VENUS_YEAR_IN_SECONDS).round(2)
+  # end
+  #
+  # def on_mars
+  #   (@args / MARS_YEAR_IN_SECONDS).round(2)
+  # end
+  #
+  # def on_jupiter
+  #   (@args / JUPITER_YEAR_IN_SECONDS).round(2)
+  # end
+  #
+  # def on_saturn
+  #   (@args / SATURN_YEAR_IN_SECONDS).round(2)
+  # end
+  #
+  # def on_uranus
+  #   (@args / URANUS_YEAR_IN_SECONDS).round(2)
+  # end
+  #
+  # def on_neptune
+  #   (@args / NEPTUNE_YEAR_IN_SECONDS).round(2)
+  # end
 end
