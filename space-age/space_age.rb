adf2bc1f4a4ca320396cc78a634b26773aff2923
tarrinros <tarindis@gmail.com>
2019-04-22 11:41:00 +0300

    Space age second iteration

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,6 +1,7 @@
 class SpaceAge
   EARTH_YEAR_IN_SECONDS = 31557600.to_f
-  solar_years = {
+  PLANETS_YEARS = {
+    earth: 1.0,
     mercury: 0.2408467,
     venus: 0.61519726,
     mars: 1.8808158,
@@ -10,45 +11,13 @@ class SpaceAge
     neptune: 164.79132
   }
 
-  ["mercury", "venus", "mars", "jupiter", "saturn", "uranus", "neptune"].each do |action|
-    define_method("perform_#{action}") do |argument|
-      "performing #{action.gsub('_', ' ')} on #{argument}"
-    end
-  end
-
   def initialize(seconds)
     @seconds = seconds
   end
 
-  # def on_earth
-  #   (@args / EARTH_YEAR_IN_SECONDS).round(2)
-  # end
-  #
-  # def on_mercury
-  #   (@args / MERCURY_YEAR_IN_SECONDS).round(2)
-  # end
-  #
-  # def on_venus
-  #   (@args / VENUS_YEAR_IN_SECONDS).round(2)
-  # end
-  #
-  # def on_mars
-  #   (@args / MARS_YEAR_IN_SECONDS).round(2)
-  # end
-  #
-  # def on_jupiter
-  #   (@args / JUPITER_YEAR_IN_SECONDS).round(2)
-  # end
-  #
-  # def on_saturn
-  #   (@args / SATURN_YEAR_IN_SECONDS).round(2)
-  # end
-  #
-  # def on_uranus
-  #   (@args / URANUS_YEAR_IN_SECONDS).round(2)
-  # end
-  #
-  # def on_neptune
-  #   (@args / NEPTUNE_YEAR_IN_SECONDS).round(2)
-  # end
+  PLANETS_YEARS.each_pair do |key, value|
+    define_method("on_#{key}") do
+      (@seconds / (value * EARTH_YEAR_IN_SECONDS)).round(2)
+    end
+  end
 end
