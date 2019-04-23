86ae728a404f662fee89c64ef6b9680d1dfacb31
tarrinros <tarindis@gmail.com>
2019-04-23 20:40:27 +0300

    Refactor Triangle exercise

--- triangle/triangle.rb
+++ triangle/triangle.rb
@@ -1,38 +1,30 @@
 class Triangle
   def initialize(sides)
     @sides = sides
-    @triangular_shape = is_triangle?(sides)
   end
 
   def equilateral?
-    if @triangular_shape
-      @sides.all? { |side| side == @sides.max }
-    else
-      return false
-    end
+    return false unless is_triangle?
+
+    @sides.all? { |side| side == @sides.first }
   end
 
   def isosceles?
-    if @triangular_shape
-      equal_sides = @sides.select { |side| @sides.count(side) > 1 }
+    return false unless is_triangle?
+    equal_sides = @sides.select { |side| @sides.count(side) > 1 }
 
-      equal_sides.count >= 2 ? true : false
-    else
-     return false
-    end
+    equal_sides.count >= 2 ? true : false
   end
 
   def scalene?
-    if @triangular_shape
-      @sides.uniq.count == 3 ? true : false
-    else
-      return false
-    end
+    return false unless is_triangle?
+
+    @sides.uniq.count == 3 ? true : false
   end
 
   private
 
-  def is_triangle?(sides)
-    sides.count == 3 && sides.all? { |side| side > 0 } && sides.max <= sides.min(2).sum
+  def is_triangle?
+    @sides.count == 3 && @sides.all? { |side| side > 0 } && @sides.max <= @sides.min(2).sum
   end
 end
