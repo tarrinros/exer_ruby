b6cae914fad91afd3e2580ac7e71c5a12db62b50
tarrinros <tarindis@gmail.com>
2019-04-26 07:24:31 +0300

    RNA transcription second iteration

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -2,6 +2,6 @@ class Complement
   DNA_RNA_PAIRS = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}.freeze
 
   def self.of_dna(args)
-    args.scan(/[[:alpha:]]/).map { |i| DNA_RNA_PAIRS.fetch(i, '') }.join
+    args.gsub(/[[:alpha:]]/) { |i| DNA_RNA_PAIRS[i] }
   end
 end
