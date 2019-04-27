655ce0e08f439efb74fff747ac4c47a0224cb756
tarrinros <tarindis@gmail.com>
2019-04-27 17:27:08 +0300

    Series first pass

--- series/series.rb
+++ series/series.rb
@@ -8,12 +8,6 @@ class Series
   def slices(amount)
     raise ArgumentError.new unless self.args.length >= amount
 
-    slice_consecutive(amount)
-  end
-
-  private
-
-  def slice_consecutive(amount)
-    self.args.chars.each_cons(amount).map(&:join)
+    args.chars.each_cons(amount).map(&:join)
   end
 end
