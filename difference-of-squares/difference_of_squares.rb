c19c136388f1974d31b8107a21f3e5b925f3a70a
tarrinros <tarindis@gmail.com>
2019-04-28 13:23:15 +0300

    Difference of squares refactor

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,28 +1,27 @@
 class Squares
-  attr_reader :threshold
-  private :threshold
-
   def initialize(threshold)
     @threshold = threshold
   end
 
   def square_of_sum
-    sum = 0
-
-    1.upto(threshold) { |i| sum += i }
-
-    sum ** 2
+    make_sequence.sum ** 2
   end
 
   def sum_of_squares
-    sum = 0
-
-    1.upto(threshold) { |i| sum += i ** 2 }
-
-    sum
+    make_sequence.map { |i| i**2 }.sum
   end
 
   def difference
     square_of_sum - sum_of_squares
+  end
+
+  private
+
+  attr_reader :threshold
+
+  def make_sequence
+    sequence = []
+    1.upto(threshold) { |i| sequence.push(i) }
+    sequence
   end
 end
