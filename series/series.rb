b9052d90c3b65c9970d00bd501e4643500a50e4a
tarrinros <tarindis@gmail.com>
2019-04-28 13:24:14 +0300

    Assign attr_reader as private

--- series/series.rb
+++ series/series.rb
@@ -1,7 +1,4 @@
 class Series
-  attr_reader :digits_sequence
-  private :digits_sequence
-
   def initialize(digits_sequence)
     @digits_sequence = digits_sequence
   end
@@ -11,4 +8,8 @@ class Series
 
     digits_sequence.chars.each_cons(elements_amount).map(&:join)
   end
+
+  private
+
+  attr_reader :digits_sequence
 end
