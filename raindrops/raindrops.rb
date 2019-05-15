4afe0c93ab52f4c2f6466e19ca0705724c5deb12
tarrinros <tarindis@gmail.com>
2019-05-15 20:49:31 +0300

    Raindrops second pass

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,8 +1,12 @@
 class Raindrops
-  FACTORS = {3 => 'Pling', 5 => 'Plang', 7 => 'Plong'}
+  RULES = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong'
+  }
 
   def self.convert(number)
-    result = (1..number).map { |i| FACTORS[i] if number % i == 0 }.join
+    result = RULES.map { |key, value| value if number % key == 0 }.join
 
     result.empty? ? number.to_s : result
   end
