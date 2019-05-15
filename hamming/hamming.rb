2f3395930a87c7bfabab265644c2fe6cb672b548
tarrinros <tarindis@gmail.com>
2019-05-15 13:00:52 +0300

    Hamming second iteration

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,15 +1,8 @@
 class Hamming
   def self.compute(first_dna, second_dna)
-    first_dna = first_dna.split("")
-    second_dna = second_dna.split("")
 
-    raise ArgumentError.new if first_dna.length != second_dna.length
+    raise ArgumentError if first_dna.length != second_dna.length
 
-    result = 0
-
-    first_dna.each_index do |i|
-      result += 1 unless first_dna[i].eql?(second_dna[i])
-    end
-    result
+    first_dna.chars.each_index.count { |i| first_dna[i] != second_dna.chars[i] }
   end
 end
