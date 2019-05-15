46b374df17e5814cc15c531719accad3c310fefc
tarrinros <tarindis@gmail.com>
2019-05-15 14:27:43 +0300

    Sum of multiplies second iteration

--- sum-of-multiples/sum_of_multiples.rb
+++ sum-of-multiples/sum_of_multiples.rb
@@ -3,25 +3,15 @@ class SumOfMultiples
     @numbers = numbers
   end
 
-  def to(threshold)
-    # result = []
-    #
-    # 1.upto(threshold - 1) do |a|
-    #   result.push(a) if numbers.any? { |b| (a % b).zero? }
-    # end
-    #
-    # result.sum
-
-    1.upto(threshold - 1).filter do |a|
-      numbers.any? do |b|
-        (a % b).zero?
-      end
-    end.sum
+  def to(thershold)
+    (1...thershold).filter { |x| multiples?(x) }.sum
   end
 
   private
 
   attr_reader :numbers
-end
 
-puts SumOfMultiples.new(0).to(2)
+  def multiples?(x)
+    numbers.any? { |y| (x % y).zero? }
+  end
+end
