a7b86215f95bbd2bc7dd40afb0df7fbcc748dcec
tarrinros <tarindis@gmail.com>
2019-05-23 23:45:09 +0300

    Grains 4d pass with class instance

--- grains/grains.rb
+++ grains/grains.rb
@@ -2,16 +2,32 @@ class Grains
   CHESS_BOARD = (1..64)
 
   def self.square(location)
-    self.validate_location(location)
-
-    (2 ** location) / 2
+    new(location).count_grains_at_location
   end
 
   def self.total
-    (2 ** CHESS_BOARD.last) - 1
+    new(CHESS_BOARD.last).summarize_grains
   end
 
-  def self.validate_location(location)
+  def initialize(location)
+    validate_location(location)
+
+    @location = location
+  end
+
+  def count_grains_at_location
+    (2 ** location) / 2
+  end
+
+  def summarize_grains
+    (2 ** location) - 1
+  end
+
+  private
+
+  attr_reader :location
+
+  def validate_location(location)
     raise BoardLocationError unless CHESS_BOARD.cover?(location)
   end
 end
