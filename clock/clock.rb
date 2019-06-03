8a4689236b29d23b3c6aaac2e416b0641b66d07f
tarrinros <tarindis@gmail.com>
2019-06-03 21:44:25 +0300

    Time parser for clock

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,5 +1,4 @@
 require 'time'
-require 'pry'
 
 Clock = Struct.new(:hour, :minute) do
   def initialize(hour: 0, minute: 0)
@@ -7,8 +6,20 @@ Clock = Struct.new(:hour, :minute) do
   end
 
   def to_s
-   Time.parse("#{hour}:#{minute}").strftime("%H:%M")
+   Time.parse("#{convert_hour}:#{convert_minute}").strftime("%H:%M")
+  end
+
+  private
+
+  def convert_hour
+    (hour % 24 + m_to_h) % 24
+  end
+
+  def convert_minute
+    minute % 60
+  end
+
+  def m_to_h
+    (((minute - (minute % 60)) / 60) % 24)
   end
 end
-
-puts Clock.new(hour: 8).to_s
