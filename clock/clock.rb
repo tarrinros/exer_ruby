8490d756ac2bafdad69fd64bd5401ccf7d7ac245
tarrinros <tarindis@gmail.com>
2019-06-03 17:58:45 +0300

    Clock first iteration

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,5 +1,14 @@
-require "time"
+require 'time'
+require 'pry'
 
-class Clock
+Clock = Struct.new(:hour, :minute) do
+  def initialize(hour: 0, minute: 0)
+    super(hour, minute)
+  end
 
+  def to_s
+   Time.parse("#{hour}:#{minute}").strftime("%H:%M")
+  end
 end
+
+puts Clock.new(hour: 8).to_s
--- clock/clock_test.rb
+++ clock/clock_test.rb
@@ -9,32 +9,32 @@ class ClockTest < Minitest::Test
   end
 
   def test_past_the_hour
-    skip
+    # skip
     assert_equal "11:09", Clock.new(hour: 11, minute: 9).to_s
   end
 
   def test_midnight_is_zero_hours
-    skip
+    # skip
     assert_equal "00:00", Clock.new(hour: 24).to_s
   end
 
   def test_hour_rolls_over
-    skip
+    # skip
     assert_equal "01:00", Clock.new(hour: 25).to_s
   end
 
   def test_hour_rolls_over_continuously
-    skip
+    # skip
     assert_equal "04:00", Clock.new(hour: 100).to_s
   end
 
   def test_sixty_minutes_is_next_hour
-    skip
+    # skip
     assert_equal "02:00", Clock.new(hour: 1, minute: 60).to_s
   end
 
   def test_minutes_roll_over
-    skip
+    # skip
     assert_equal "02:40", Clock.new(minute: 160).to_s
   end
 
