d2f66f59baeb8b68c5c7203eae3656a2c0084174
tarrinros <tarindis@gmail.com>
2019-06-11 10:50:30 +0300

    Add Format constant

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,11 +1,14 @@
 class Clock
-  MIN_IN_HOUR = 60
-  MIN_IN_DAY = 1440
-
-  attr_reader :time_in_minutes
+  MINUTES_IN_HOUR = 60
+  MINUTES_IN_DAY = 1440
+  TIME_FORMAT = '%02d:%02d'
 
   def initialize(hour: 0, minute: 0)
-    @time_in_minutes = count_time(hour, minute)
+    @time_in_minutes = normalize_to_minutes(hour, minute)
+  end
+
+  def ==(clock)
+    time_in_minutes == clock.time_in_minutes
   end
 
   def +(clock)
@@ -16,17 +19,17 @@ class Clock
     Clock.new(minute: time_in_minutes - clock.time_in_minutes)
   end
 
-  def ==(clock)
-    time_in_minutes == clock.time_in_minutes
-  end
-
   def to_s
-    '%02d:%02d' % time_in_minutes.divmod(MIN_IN_HOUR)
+    TIME_FORMAT % time_in_minutes.divmod(MINUTES_IN_HOUR)
   end
 
   private
 
-  def count_time(hour, minute)
-    (hour * MIN_IN_HOUR + minute) % MIN_IN_DAY
+  def normalize_to_minutes(hour, minute)
+    (hour * MINUTES_IN_HOUR + minute) % MINUTES_IN_DAY
   end
+
+  protected
+
+  attr_reader :time_in_minutes
 end
