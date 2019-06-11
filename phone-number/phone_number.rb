fe3a0bdedebfcd2943d540146b5e98821aa1f281
tarrinros <tarindis@gmail.com>
2019-06-11 22:03:13 +0300

    Phone number first pass

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -1,17 +1,23 @@
 class PhoneNumber
-  def self.clean(phone)
-    new(phone).clean
+  CODE_PATTERN = /\A([2-9]\d\d){2}(\d){4}\z/
+
+  def self.clean(number)
+    new(number).clean
   end
 
-  def initialize(phone)
-    @phone = phone
+  def initialize(number)
+    @parsed_number = parse_number(number)
+  end
+
+  def clean
+    parsed_number =~ CODE_PATTERN ? parsed_number : nil
   end
 
   private
 
-  attr_reader :phone
+  attr_reader :parsed_number
 
-  def clean
-
+  def parse_number(number)
+    number.scan(/[[:digit:]]/).join.sub(/\A1/,'')
   end
 end
