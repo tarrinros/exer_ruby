d44e32f24052a2bd16682dc57a1903251cba0fc1
tarrinros <tarindis@gmail.com>
2019-06-13 23:46:06 +0300

    Saddle point rows and columns method

--- saddle-points/saddle_points.rb
+++ saddle-points/saddle_points.rb
@@ -1,17 +1,21 @@
 class Matrix
-  def initialize
-
+  def initialize(string)
+    @matrix = string.lines(chomp: true).map { |row| row.split.map(&:to_i) }
   end
 
   def rows
-
+    matrix
   end
 
   def columns
-
+    matrix.transpose
   end
 
   def saddle_points
 
   end
+
+  private
+
+  attr_reader :matrix
 end
